bd40029290a32286387de0b4c4c50d72e81a65c2
Adam Butler <adamjbutler091@gmail.com>
2022-10-10 17:06:39 +0930

    puppy image component

--- todo_idom_py/__main__.py
+++ todo_idom_py/__main__.py
@@ -12,4 +12,14 @@ def HelloWorld():
     )
 )
 
-run(HelloWorld)
+@component
+def Photo():
+    return html.img(
+        {
+            "src": "https://picsum.photos/id/237/500/300",
+            "style": {"width": "50%"},
+            "alt": "Puppy",
+        }
+    )
+
+run(Photo)
