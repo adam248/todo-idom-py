29227bb9ba7393a7d3435258cde65ae8b404703f
Adam Butler <adamjbutler091@gmail.com>
2022-10-10 16:10:07 +0930

    basic html title and list

--- todo_idom_py/__main__.py
+++ todo_idom_py/__main__.py
@@ -3,6 +3,13 @@ from idom import component, html, run
 
 @component
 def HelloWorld():
-    return html.h1("Hello, World!")
+    return html.div(
+    html.h1("My Todo List"),
+    html.ul(
+        html.li("Design a cool new app"),
+        html.li("Build it"),
+        html.li("Share it with the world!"),
+    )
+)
 
 run(HelloWorld)
