c0e69c29befe94055cf76f9ca0047d92a25a5bee
Adam Butler <adamjbutler091@gmail.com>
2022-10-10 17:23:02 +0930

    dynamic list rendering of data

--- todo_idom_py/__main__.py
+++ todo_idom_py/__main__.py
@@ -2,24 +2,31 @@ from idom import component, html, run
 
 
 @component
-def HelloWorld():
-    return html.div(
-    html.h1("My Todo List"),
-    html.ul(
-        html.li("Design a cool new app"),
-        html.li("Build it"),
-        html.li("Share it with the world!"),
-    )
-)
+def DataList(items, filter_by_priority=None, sort_by_priority=False):
+    if filter_by_priority:
+        items = [i for i in items if i["priority"] <= filter_by_priority]
+    if sort_by_priority:
+        items = list(sorted(items, key=lambda x: x["priority"]))
+    list_item_elements = [html.li(i["text"], key=i["id"]) for i in items]
+    return html.ul(list_item_elements)
+
 
 @component
-def Photo():
-    return html.img(
-        {
-            "src": "https://picsum.photos/id/237/500/300",
-            "style": {"width": "50%"},
-            "alt": "Puppy",
-        }
+def TodoList():
+    tasks = [
+        {"id": 0, "text": "Make breakfast", "priority": 0},
+        {"id": 1, "text": "Feed the dog", "priority": 0},
+        {"id": 2, "text": "Do laundry", "priority": 2},
+        {"id": 3, "text": "Go on a run", "priority": 1},
+        {"id": 4, "text": "Clean the house", "priority": 2},
+        {"id": 5, "text": "Go to the grocery store", "priority": 2},
+        {"id": 6, "text": "Do some coding", "priority": 1},
+        {"id": 7, "text": "Read a book", "priority": 1},
+        ]
+
+    return html.section(
+        html.h1("My Todo List"),
+        DataList(tasks, filter_by_priority=1, sort_by_priority=True)
     )
 
-run(Photo)
+run(TodoList)
