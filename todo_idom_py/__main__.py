869e2435837f443fad6dcae25d30e5ea72c409ea
Adam Butler <adamjbutler091@gmail.com>
2022-10-12 19:27:24 +0930

    removing examples to focus on the todo list component

--- todo_idom_py/__main__.py
+++ todo_idom_py/__main__.py
@@ -1,19 +1,11 @@
 from idom import component, html, run
 
-from .counter import Counter
-from .gallery import Gallery, ScrollingGallery
-from .moving import MovingDot
-from .playpause import PlayPauseApp
 from .todo import TodoList
 
 
 @component
 def Layout():
     return html.div(
-        MovingDot(),
-        Counter(),
-        ScrollingGallery(),
-        PlayPauseApp(),
         TodoList(),
     )
 
